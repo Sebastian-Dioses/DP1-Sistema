749f8c4cc98e786a13e7c561161934efffd0cd21
Sebastian-Dioses <sebastian.dioses@gmail.com>
2016-10-24 09:09:58 -0500

    Sorry

---
+++ target/scala-2.10/src_managed/main/views/html/index.template.scala
@@ -0,0 +1,57 @@
+
+package views.html
+
+import play.templates._
+import play.templates.TemplateMagic._
+
+import play.api.templates._
+import play.api.templates.PlayMagic._
+import models._
+import controllers._
+import java.lang._
+import java.util._
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+import play.api.i18n._
+import play.core.j.PlayMagicForJava._
+import play.mvc._
+import play.data._
+import play.api.data.Field
+import play.mvc.Http.Context.Implicit._
+import views.html._
+/**/
+object index extends BaseScalaTemplate[play.api.templates.HtmlFormat.Appendable,Format[play.api.templates.HtmlFormat.Appendable]](play.api.templates.HtmlFormat) with play.api.templates.Template1[String,play.api.templates.HtmlFormat.Appendable] {
+
+    /**/
+    def apply/*1.2*/(message: String):play.api.templates.HtmlFormat.Appendable = {
+        _display_ {
+
+Seq[Any](format.raw/*1.19*/("""
+
+"""),_display_(Seq[Any](/*3.2*/main("Welcome to Play Framework")/*3.35*/ {_display_(Seq[Any](format.raw/*3.37*/("""
+
+    <div class="well">
+        <h1>"""),_display_(Seq[Any](/*6.14*/message)),format.raw/*6.21*/("""</h1>
+    </div>
+
+""")))})),format.raw/*9.2*/("""
+"""))}
+    }
+    
+    def render(message:String): play.api.templates.HtmlFormat.Appendable = apply(message)
+    
+    def f:((String) => play.api.templates.HtmlFormat.Appendable) = (message) => apply(message)
+    
+    def ref: this.type = this
+
+}
+                /*
+                    -- GENERATED --
+                    DATE: Sun Oct 23 19:08:25 COT 2016
+                    SOURCE: C:/Users/Sebastián/hello-play-java/app/views/index.scala.html
+                    HASH: f7ff185d4050f2eb79360e811e533c548858f5f6
+                    MATRIX: 774->1|885->18|922->21|963->54|1002->56|1075->94|1103->101|1152->120
+                    LINES: 26->1|29->1|31->3|31->3|31->3|34->6|34->6|37->9
+                    -- GENERATED --
+                */
+            
