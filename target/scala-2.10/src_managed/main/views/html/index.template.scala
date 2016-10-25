e96027c6ffe01d730bc8cebaf8a9303d23d82188
marcelomilera <m.milera@pucp.pe>
2016-10-24 23:15:07 -0500

    titulo

--- target/scala-2.10/src_managed/main/views/html/index.template.scala
+++ target/scala-2.10/src_managed/main/views/html/index.template.scala
@@ -48,29 +48,24 @@ Seq[Any](format.raw/*1.17*/("""
 	
 
 	<!---Cuerpo -->
-
-
-	<div class="content" style="max-width: 100%;">
-		<!-- Utilizando Bootstrap -->			
-		<br/><br/>
-		<div class="container">
-			<div class="row">
-	  			<div class="col-sm-12 withoutpadding" >
-	                <img style="text-align:center;margin: 0 auto;display: block;" class="img-responsive" src=""""),_display_(Seq[Any](/*31.109*/routes/*31.115*/.Assets.at("images/simulation.gif"))),format.raw/*31.150*/(""""/>
-	  			</div>
-			</div>
-		</div>			
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12 text-center">
-				<div class="header">
-				  <h1 style="color:green">Bienvenido <strong>USER</strong></h1>
-				  <h2 style="color:green">usted se ha logueado como <strong>administrador general</strong> del Club Pappus</h2>
-				</div>
-				
+				<p class="lead"><strong>SIMULACIÓN</strong></p>				
 			</div>
-		</div>			
-	</div>	
+		</div>
+	</div>
+
+	<div class="content" style="max-width: 100%;">
+		<!-- Utilizando Bootstrap -->			
+		<br/>
+		<div class="container">
+			<div class="row">
+	  			<div class="col-sm-12 withoutpadding" >
+	                <img style="text-align:center;margin: 0 auto;display: block;" class="img-responsive" src=""""),_display_(Seq[Any](/*37.109*/routes/*37.115*/.Assets.at("images/simulation.gif"))),format.raw/*37.150*/(""""/>
+	  			</div>
+			</div>
+		</div>				
 	</div>		
 	
 
@@ -168,29 +163,29 @@ Seq[Any](format.raw/*1.17*/("""
 	<!--Pie de págna-->
 		
 	<!-- JQuery -->
-		<script src='"""),_display_(Seq[Any](/*143.17*/routes/*143.23*/.Assets.at("js/jquery-1.11.3.min.js"))),format.raw/*143.60*/("""'></script>
+		<script src='"""),_display_(Seq[Any](/*138.17*/routes/*138.23*/.Assets.at("js/jquery-1.11.3.min.js"))),format.raw/*138.60*/("""'></script>
 		<!-- Bootstrap -->
-		<script src='"""),_display_(Seq[Any](/*145.17*/routes/*145.23*/.Assets.at("js/bootstrap.js"))),format.raw/*145.52*/("""'></script>	
+		<script src='"""),_display_(Seq[Any](/*140.17*/routes/*140.23*/.Assets.at("js/bootstrap.js"))),format.raw/*140.52*/("""'></script>	
 		<!-- BXSlider -->
-		<script src='"""),_display_(Seq[Any](/*147.17*/routes/*147.23*/.Assets.at("js/jquery.bxslider.min.js"))),format.raw/*147.62*/("""'></script>		
+		<script src='"""),_display_(Seq[Any](/*142.17*/routes/*142.23*/.Assets.at("js/jquery.bxslider.min.js"))),format.raw/*142.62*/("""'></script>		
 		<!-- Mis Scripts -->
-		<script src='"""),_display_(Seq[Any](/*149.17*/routes/*149.23*/.Assets.at("js/MisScripts.js"))),format.raw/*149.53*/("""'></script>
+		<script src='"""),_display_(Seq[Any](/*144.17*/routes/*144.23*/.Assets.at("js/MisScripts.js"))),format.raw/*144.53*/("""'></script>
 
-		<script src='"""),_display_(Seq[Any](/*151.17*/routes/*151.23*/.Assets.at("js/jquery.dataTables.js"))),format.raw/*151.60*/("""'></script>
+		<script src='"""),_display_(Seq[Any](/*146.17*/routes/*146.23*/.Assets.at("js/jquery.dataTables.js"))),format.raw/*146.60*/("""'></script>
 		
 		<script>
-		$(document).ready(function() """),format.raw/*154.32*/("""{"""),format.raw/*154.33*/("""
-		   $('#example').DataTable( """),format.raw/*155.31*/("""{"""),format.raw/*155.32*/("""
-		       "language": """),format.raw/*156.22*/("""{"""),format.raw/*156.23*/("""
+		$(document).ready(function() """),format.raw/*149.32*/("""{"""),format.raw/*149.33*/("""
+		   $('#example').DataTable( """),format.raw/*150.31*/("""{"""),format.raw/*150.32*/("""
+		       "language": """),format.raw/*151.22*/("""{"""),format.raw/*151.23*/("""
 		           "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json"
-		       """),format.raw/*158.10*/("""}"""),format.raw/*158.11*/("""
-		  	"""),format.raw/*159.6*/("""}"""),format.raw/*159.7*/(""");
-  		"""),format.raw/*160.5*/("""}"""),format.raw/*160.6*/(""");
+		       """),format.raw/*153.10*/("""}"""),format.raw/*153.11*/("""
+		  	"""),format.raw/*154.6*/("""}"""),format.raw/*154.7*/(""");
+  		"""),format.raw/*155.5*/("""}"""),format.raw/*155.6*/(""");
 		</script>
 	</body>
 	</html>
 
-""")))})),format.raw/*165.2*/("""
+""")))})),format.raw/*160.2*/("""
 """))}
     }
     
@@ -203,11 +198,11 @@ Seq[Any](format.raw/*1.17*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Mon Oct 24 18:21:56 COT 2016
+                    DATE: Mon Oct 24 23:13:02 COT 2016
                     SOURCE: C:/Users/MARCELO/Documents/GitHub/DP1-Sistema/app/views/index.scala.html
-                    HASH: ee21f82078c3f71152b84d1096a4f40fafdf562a
-                    MATRIX: 774->1|883->16|922->21|936->28|989->73|1028->75|1271->282|1286->288|1345->325|1417->361|1432->367|1486->399|1558->435|1573->441|1626->472|1698->508|1713->514|1769->548|1842->585|1857->591|1910->622|2304->979|2320->985|2378->1020|6061->4666|6077->4672|6137->4709|6225->4760|6241->4766|6293->4795|6381->4846|6397->4852|6459->4891|6551->4946|6567->4952|6620->4982|6688->5013|6704->5019|6764->5056|6853->5116|6883->5117|6944->5149|6974->5150|7026->5173|7056->5174|7179->5268|7209->5269|7244->5276|7273->5277|7309->5285|7338->5286|7410->5326
-                    LINES: 26->1|29->1|31->3|31->3|31->3|31->3|40->12|40->12|40->12|41->13|41->13|41->13|42->14|42->14|42->14|43->15|43->15|43->15|44->16|44->16|44->16|59->31|59->31|59->31|171->143|171->143|171->143|173->145|173->145|173->145|175->147|175->147|175->147|177->149|177->149|177->149|179->151|179->151|179->151|182->154|182->154|183->155|183->155|184->156|184->156|186->158|186->158|187->159|187->159|188->160|188->160|193->165
+                    HASH: ee2c5cd3c2f7c79fb36ad9028c594bc75f7826bc
+                    MATRIX: 774->1|883->16|922->21|936->28|989->73|1028->75|1271->282|1286->288|1345->325|1417->361|1432->367|1486->399|1558->435|1573->441|1626->472|1698->508|1713->514|1769->548|1842->585|1857->591|1910->622|2471->1146|2487->1152|2545->1187|5878->4483|5894->4489|5954->4526|6042->4577|6058->4583|6110->4612|6198->4663|6214->4669|6276->4708|6368->4763|6384->4769|6437->4799|6505->4830|6521->4836|6581->4873|6670->4933|6700->4934|6761->4966|6791->4967|6843->4990|6873->4991|6996->5085|7026->5086|7061->5093|7090->5094|7126->5102|7155->5103|7227->5143
+                    LINES: 26->1|29->1|31->3|31->3|31->3|31->3|40->12|40->12|40->12|41->13|41->13|41->13|42->14|42->14|42->14|43->15|43->15|43->15|44->16|44->16|44->16|65->37|65->37|65->37|166->138|166->138|166->138|168->140|168->140|168->140|170->142|170->142|170->142|172->144|172->144|172->144|174->146|174->146|174->146|177->149|177->149|178->150|178->150|179->151|179->151|181->153|181->153|182->154|182->154|183->155|183->155|188->160
                     -- GENERATED --
                 */
             
