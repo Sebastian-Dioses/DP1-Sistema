d146593e13f80f5db7b60a2c29ad446251c092b0
marcelomilera <m.milera@pucp.pe>
2016-11-13 18:44:11 -0500

    Agregado Scripts de creacion de BD

---
+++ BD/1.dp1.ciudades.sql
@@ -0,0 +1,46 @@
+/*
+-- Query: SELECT * FROM dp1.ciudades
+LIMIT 0, 1000
+
+-- Date: 2016-11-08 17:54
+*/
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('BIKF','Reykjavik','Islandia','reyk',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('EBCI','Bruselas','Belgica','brus',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('EDDI','Berlin','Alemania','berl',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('EETN','Tallin','Estonia','tall',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('EFHK','Helsinki','Finlandia','hels',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('EGLL','Londres','Reino Unido','lond',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('EHAM','Amsterdam','Holanda','amst',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('EIDW','Dublin','Irlanda (Eire)','dubl',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('EKCH','Copenhague','Dinamarca','cope',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('ELLX','Luxemburgo','Luxemburgo','luxe',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('ENGM','Oslo','Noruega','oslo',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('EPMO','Varsovia','Polonia','vars',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('ESKN','Estocolmo','Suecia','esto',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('EVRA','Riga','Letonia','riga',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('LATI','Tirana','Albania','tira',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('LBSF','Sofia','Bulgaria','sofi',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('LDZA','Zagreb','Croacia','zagr',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('LEMD','Madrid','España','madr',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('LFPG','Paris','Francia','pari',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('LGAV','Atenas','Grecia','aten',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('LHBP','Budapest','Hungría','buda',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('LIRA','Roma','Italia','roma',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('LJLJ','Liubliana','Eslovenia','liub',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('LKPR','Praga','Checa','prag',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('LMML','LaValletta','Malta','lava',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('LOWW','Viena','Austria','vien',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('LPPT','Lisboa','Portugal','lisb',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('LSZB','Berna','Suiza','bern',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('LZIB','Bratislava','Eslovaquia','brat',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('SABE','Buenos Aires','Argentina','buen',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('SBBR','Brasilia','Brasil','bras',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('SCEL','Santiago de Chile','Chile','sant',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('SEQM','Quito','Ecuador','quit',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('SGAS','Asunción','Paraguay','asun',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('SKBO','Bogota','Colombia','bogo ',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('SLLP','La Paz','Bolivia','lapa',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('SPIM','Lima','Perú','lima',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('SUAA','Motenvideo','Uruguay','mont',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('SVMI','Caracas','venezuela','cara',600);
+INSERT INTO `ciudades` (`cod_ciudad`,`nombre`,`pais`,`abreviado`,`capacidad_almacen`) VALUES ('UMMS','Minsk','Bielorrusia','mins',600);
---
+++ BD/2.dp1.personas.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM dp1.personas
+LIMIT 0, 1000
+
+-- Date: 2016-11-08 17:56
+*/
+INSERT INTO `personas` (`id`,`nombre`,`dni`,`correo`,`tipo_persona`) VALUES (1,'Marcelo Milera','18273941','m.milera@pucp.pe','1');
+INSERT INTO `personas` (`id`,`nombre`,`dni`,`correo`,`tipo_persona`) VALUES (2,'Sebastian Dioses','19374728','s.dioses@pucp.pe','1');
+INSERT INTO `personas` (`id`,`nombre`,`dni`,`correo`,`tipo_persona`) VALUES (3,'Nilton Guerra','29437362','guerra.nilton@pucp.pe','1');
+INSERT INTO `personas` (`id`,`nombre`,`dni`,`correo`,`tipo_persona`) VALUES (4,'Jose Luis Gil','37462514','josegil@pucp.pe','1');
