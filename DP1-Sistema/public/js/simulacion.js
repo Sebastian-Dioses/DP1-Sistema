820c10a50d0e5ea790131b6b017b298e13b889b5
nilot111 <guerra.n@pucp.pe>
2016-11-27 15:24:24 -0500

    socketsFront

--- DP1-Sistema/public/js/simulacion.js
+++ DP1-Sistema/public/js/simulacion.js
@@ -1,91 +1,7 @@
+var websocket;
 
-///////////////////// OBTENER CIUDADES
-var  cities= new Map();
-
-var oTable = document.getElementById('ciudades_tabla');
-//gets rows of table
-var rowLength = oTable.rows.length;
-
-//loops through rows    
-for (i = 0; i < rowLength; i++){
-
-   //gets cells of current row
-   var ciudadFila = oTable.rows.item(i).cells;
-
-   var codCiud=ciudadFila.item(0).innerHTML;
-   var city= {
-      codigo: ciudadFila.item(0).innerHTML,
-      nombre: ciudadFila.item(1).innerHTML,
-      pais: ciudadFila.item(2).innerHTML,
-      latitud: ciudadFila.item(3).innerHTML,
-      longitud: ciudadFila.item(4).innerHTML,
-      huso: ciudadFila.item(5).innerHTML
-   }
-   cities.set(codCiud,city);
-}
-////////OBTENER VUELOS
-var vuelos = new Array();
-
-var oTableV = document.getElementById('vuelos_tabla');
-//gets rows of table
-var rowLengthV = oTableV.rows.length;
-
-//loops through rows    
-for (i = 0; i < rowLengthV; i++){
-  var vueloFila = oTableV.rows.item(i).cells;
-  //Procesamos la hora
-  var dt = new Date(vueloFila.item(3).innerHTML);
-  var horaSalida = dt.getUTCHours();
-  var husoO=cities.get(vueloFila.item(1).innerHTML).huso;
-  //console.log(horaSalida+"-"+husoO);
-  var horaSalidaEstandar=horaSalida-husoO;
-  if(horaSalidaEstandar<0) horaSalidaEstandar+=24;
-  horaSalidaEstandar%=24;
-  var dt2 = new Date(vueloFila.item(4).innerHTML);
-  var horaLlegada = dt2.getUTCHours();
-  var husoF=cities.get(vueloFila.item(2).innerHTML).huso;
-  var horaLlegadaEstandar=horaLlegada-husoF;
-  if(horaLlegadaEstandar<0) horaLlegadaEstandar+=24;
-  horaLlegadaEstandar%=24;  
-  var tiempo=horaLlegadaEstandar-horaSalidaEstandar;
-  if(tiempo<0) tiempo+=24;
-  var ciudadOri=cities.get(vueloFila.item(1).innerHTML);
-  var ciudadFin=cities.get(vueloFila.item(2).innerHTML);
-  var sla=(ciudadOri.latitud-ciudadFin.latitud)/200;
-  var slo= (ciudadOri.longitud-ciudadFin.longitud)/200;
-  console.log("SLA: "+sla+" SLO: "+slo);
-    var vuelo={
-        codigo: vueloFila.item(0).innerHTML,
-        ciudadO: vueloFila.item(1).innerHTML,
-        ciudadF: vueloFila.item(2).innerHTML,
-        horaSalida: horaSalidaEstandar,
-        horaLlegada: horaLlegadaEstandar,
-        stepLatitud: sla,
-        stepLongitud: slo,
-        tiempo: tiempo
-    }
-   vuelos.push(vuelo);
-   //console.log(vuelos[i]);
-}
-vuelos.sort(function (a, b) { //ordenamos por hora :v
-  if (a.horaSalida > b.horaSalida) {
-    return 1;
-  }
-  if (a.horaSalida < b.horaSalida) {
-    return -1;
-  }
-  // a must be equal to b
-  return 0;
-});
-var indicesVuelos= new Array(); //indices de vuelos segun hora
-indicesVuelos.push(0);// marcamos el inicio
-var ini=0;
-for(var n=0;n<vuelos.length; n++){
-    if(vuelos[n].horaSalida!=ini) {
-      indicesVuelos.push(n);
-      ini=vuelos[n].horaSalida;
-    }  
-}
+var websocketEchoServerUri = "ws://localhost:9000/socket";
+var serverLog = document.getElementById("maplog");
 
 ///////GRAFICOS PARA LA SIMULACION (INICIALIZACION)
 AmCharts.ready(function() {
@@ -127,11 +43,52 @@ var targetSVG = "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z 
   map.dataProvider = dataProvider;
 
   map.write("chartdiv");
+  websocket = initWebSocket(websocketEchoServerUri);
 });
 
-/**
- * Calculates bullet size based on its value
- */
+// Funciones del socket
+
+function initWebSocket(wsUri) {
+  var ws = new WebSocket(wsUri);
+  ws.onopen = onConnect;
+  ws.onclose = onClose;
+  ws.onerror = onError;
+  ws.onmessage = updateLog;
+  return ws;
+}
+
+
+function onConnect(wsEvent) {
+  writeToScreen("Server connection successful. Listening for data now.");
+  //interval = setInterval(getDataFromServer, 2000); //we're simulating a datafeed by calling our getDataFromServer method every 2 seconds
+}
+
+function onError(wsEvent) {
+  writeToScreen("<span style='color: red'>ERROR:" + wsEvent + "</span>");
+}
+
+function onClose(wsEvent) {
+  writeToScreen("Server connection closed");
+  clearInterval(interval);
+}
+
+//For debug messaging
+function writeToScreen(message) {
+  var pre = document.createElement("p");
+  pre.style.wordWrap = "break-word";
+  pre.innerHTML = message;
+  serverLog.appendChild(pre);
+  serverLog.scrollTop = serverLog.scrollHeight;
+}
+
+function updateLog(wsEvent) {
+  //var newData = JSON.parse(wsEvent.data);
+  //chartData.push.apply(chartData, newData);
+  // keep only 50 datapoints on screen for the demo
+
+  writeToScreen("<span style='color: blue'>Received: " + wsEvent.data + "</span>");
+  //chart.validateData(); //call to redraw the chart with new data
+}
 
 
 function mostrarResultadosRuteos(data){
