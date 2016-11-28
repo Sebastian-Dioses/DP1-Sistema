36cd85d689e1ee5e392bba27ff0c49c2c31a66c4
jlga94 <jose.gil@pucp.pe>
2016-11-27 20:32:14 -0500

    Revert "Cambios por JASON"
    This reverts commit e52b127e4c5cb4457a6ad73a7d74fc5be0613cf9.

--- DP1-Sistema/app/controllers/Application.java
+++ DP1-Sistema/app/controllers/Application.java
@@ -48,12 +48,12 @@ public class Application extends Controller {
 	
 	public static class Paquete{
 		public Long id;
-		public Long fecha;
+		public String fecha;
 		public String hora;
 		public String origen;
 		public String destino;
-		public String ruta;
-		public Long stop;
+		public Integer factible;
+		public Integer stop;
 	}
 
 	
@@ -64,7 +64,7 @@ public class Application extends Controller {
 		BufferArchivos baPedidos = BufferArchivos.getInstance();
 		
 		TreeMap<Integer,String[]> listaPedidosEscala=null;
-		if(1==1){
+		if(scale==1){
 			listaPedidosEscala = baPedidos.getListaPedidosEscala1();
 		}else if(scale==3){
 			listaPedidosEscala = baPedidos.getListaPedidosEscala2();		
@@ -72,21 +72,8 @@ public class Application extends Controller {
 			listaPedidosEscala = baPedidos.getListaPedidosEscala3();
 		}
 		
-		
 		Logger.info("Cantidad horas en escala: "+listaPedidosEscala.size());
-
-		Paquete pk = new Paquete();
 			
-		if(toIntExact(time)==listaPedidosEscala.size()){
-			Integer a=1;
-			pk.stop=a.longValue();
-			return ok(Json.toJson(pk));
-		}
-		Integer a=0;
-		pk.stop=a.longValue();
-		
-		
-		
 		String [] pedidos = null;
 		if(scale==1){
 			pedidos = listaPedidosEscala.get(toIntExact(time));
@@ -96,35 +83,46 @@ public class Application extends Controller {
 			pedidos = listaPedidosEscala.get(toIntExact(time));
 		}
 		
+		Paquete pk = new Paquete();
+		if(toIntExact(time)==listaPedidosEscala.size()){
+			pk.stop=1;//para que el front sepa que ya se debe terminar de iterar
+		}else{
+			pk.stop=0;//para que en el front sepa que se seguira iterando
+		}
+		pk.factible=0;
+
+		
 		GestorCiudades gc=GestorCiudades.getInstance();
 		
 		Logger.info("Cantidad paquetes: "+pedidos.length+"-"+time);
+		Gson gson = new Gson();
 		
-		
-		for(int i=0;i<pedidos.length;i++){
+		Boolean todosFactibles=true;
+		for(int i=0;i<pedidos.length && todosFactibles;i++){
 			String [] datosPaquete = pedidos[i].trim().split("-");//0:fecha 1:hora 2: ciudad origen 3: ciudad fin					
 			
-			RutaEscogida mejorRuta=gc.DFS(datosPaquete[3],datosPaquete[4],1,datosPaquete[2],1,datosPaquete[1]);
+			RutaEscogida mejorRuta=gc.DFS(datosPaquete[2],datosPaquete[3],1,datosPaquete[1],1,datosPaquete[0]);
 			String resultado=null;
 			if(mejorRuta.getEstadoRuta()==0){//0 es Factible
 				resultado="Numpedido: "+i+" "+pedidos[i]+" Ruta: "+ mejorRuta.imprimirRecorrido();
-				
+				String resultadoJSON=(String)gson.toJson(mejorRuta, RutaEscogida.class);
+				SimpleChat.notifyAll(resultadoJSON);//Acá se podría mandar un Json con los datos del paquete
 			}else{
-				resultado="Numpedido: "+i+" No se encontro ruta - Ciudad Origen: "+ datosPaquete[3]+" Ciudad Fin: "+datosPaquete[4];
-			}
-			
+				if(mejorRuta.getEstadoRuta()!=1){//Si se cae por condición de capacidades
+					todosFactibles=false;
+					pk.factible=mejorRuta.getEstadoRuta();
+					pk.stop=1;//para que el front sepa que ya se debe terminar de iterar
+					pk.fecha=datosPaquete[0];
+					pk.hora=datosPaquete[1];
+					pk.origen=datosPaquete[2];
+					pk.destino=datosPaquete[3];
+				}
+				resultado="Numpedido: "+i+" No se encontro ruta - Ciudad Origen: "+ datosPaquete[2]+" Ciudad Fin: "+datosPaquete[3];	
+			}			
 			Logger.info(resultado);
-			SimpleChat.notifyAll(resultado);//Acá se podría mandar un Json con los datos del paquete
 		}
-
-		
-		pk.hora="12:20";
-		pk.origen="ABCD";
-		pk.destino="EFGH";
-		
 		return ok(Json.toJson(pk));
 	}
-	
 	
 	
     public static Result login() {            
