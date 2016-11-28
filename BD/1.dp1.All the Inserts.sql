9588463fabd7fd341bbea3f35f156179c7f8f7e4
marcelomilera <m.milera@pucp.pe>
2016-11-28 15:38:14 -0500

    BD script todos los inserts corregido

--- BD/1.dp1.All the Inserts.sql
+++ BD/1.dp1.All the Inserts.sql
@@ -1149,7 +1149,7 @@ INSERT INTO `pedidos` (`id`,`ciudad_origen`,`ciudad_destino`,`fecha_registro`,`p
 INSERT INTO `pedidos` (`id`,`ciudad_origen`,`ciudad_destino`,`fecha_registro`,`personas_id`,`personas2_id`) VALUES (7,'SABE','SVMI','2016-11-15 23:24:35',14,9);
 INSERT INTO `pedidos` (`id`,`ciudad_origen`,`ciudad_destino`,`fecha_registro`,`personas_id`,`personas2_id`) VALUES (8,'SABE','LATI','2016-11-17 23:29:13',8,9);
 INSERT INTO `pedidos` (`id`,`ciudad_origen`,`ciudad_destino`,`fecha_registro`,`personas_id`,`personas2_id`) VALUES (9,'LFPG','EPMO','2016-11-26 23:31:20',9,8);
-INSERT INTO `pedidos` (`id`,`ciudad_origen`,`ciudad_destino`,`fecha_registro`,`personas_id`,`personas2_id`) VALUES (10,'LJLJ','LMML','2016-11-26 23:34:41',10,7);
+INSERT INTO `pedidos` (`id`,`ciudad_origen`,`ciudad_destino`,`fecha_registro`,`personas_id`,`personas2_id`) VALUES (10,'LJLJ','LMML','2016-11-26 22:34:41',10,7);
 INSERT INTO `pedidos` (`id`,`ciudad_origen`,`ciudad_destino`,`fecha_registro`,`personas_id`,`personas2_id`) VALUES (11,'EKCH','EETN','2016-11-27 23:42:09',7,8);
 INSERT INTO `pedidos` (`id`,`ciudad_origen`,`ciudad_destino`,`fecha_registro`,`personas_id`,`personas2_id`) VALUES (12,'LKPR','LHBP','2016-11-27 23:50:52',11,12);
 
